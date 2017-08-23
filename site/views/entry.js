15a9836d1c49eab5c153358403fecc28b3a5a6b0
jondashkyle <contact@jon-kyle.com>
2017-08-22 20:14:58 -0400

    ui

--- site/views/entry.js
+++ site/views/entry.js
@@ -8,7 +8,7 @@ function view (state, emit) {
   return html`
     <div class="pt2 pb4 px3 fs1 lh1-5 copy" sm="px1-5">
       <div style="min-height: 25vh">
-        <h1><div class="back"><a href="/">←</a></div>${state.page.title}</h1>
+        <h1>${state.page.title}</h1>
       </div>
       ${md(state.page.text)}
       <h2><a href="/"><div class="back">←</div>Index</a></h2>

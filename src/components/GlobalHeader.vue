6753ada04e07bd445cb2dd009c2f3c368e9319ad
Jon-Kyle Mohr <contact@jon-kyle.com>
2019-02-24 13:56:43 -0800

    light

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -8,13 +8,11 @@
     </nav>
     <div :class="['toggle-light', { night: isNight }]" @click="toggleLight">
       <svg width="100px" height="100px" viewBox="0 0 100 100">
-          <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <g id="orb">
-              <circle id="disc" fill="rgba(var(--fg))" cx="20" cy="20" r="20"></circle>
-              <circle id="cut" fill="rgb(var(--bg))" cx="20" cy="20" r="20"></circle>
-            </g>
-            <path d="M51,21 L48,21 L48,4 L51,4 L51,21 Z M51.75,96 L48.75,96 L48.75,79 L51.75,79 L51.75,96 Z M4.5,51.5 L4.5,48.5 L21.5,48.5 L21.5,51.5 L4.5,51.5 Z M78.5,51.5 L78.5,48.5 L95.5,48.5 L95.5,51.5 L78.5,51.5 Z M17.0841793,19.0191036 L19.2054997,16.8977832 L31.226315,28.9185985 L29.1049946,31.0399189 L17.0841793,19.0191036 Z M69.4100811,71.3450054 L71.5314015,69.223685 L83.5522168,81.2445003 L81.4308964,83.3658207 L69.4100811,71.3450054 Z M18.5691036,83.3658207 L16.4477832,81.2445003 L28.4685985,69.223685 L30.5899189,71.3450054 L18.5691036,83.3658207 Z M70.8950054,31.0399189 L68.773685,28.9185985 L80.7945003,16.8977832 L82.9158207,19.0191036 L70.8950054,31.0399189 Z" id="rays" fill="rgb(var(--fg))"></path>
+          <g id="orb">
+            <circle id="disc" fill="rgb(var(--fg))" cx="20" cy="20" r="20"></circle>
+            <circle id="cut" fill="rgb(var(--bg))" cx="20" cy="20" r="20"></circle>
           </g>
+          <path d="M51,21 L48,21 L48,4 L51,4 L51,21 Z M51.75,96 L48.75,96 L48.75,79 L51.75,79 L51.75,96 Z M4.5,51.5 L4.5,48.5 L21.5,48.5 L21.5,51.5 L4.5,51.5 Z M78.5,51.5 L78.5,48.5 L95.5,48.5 L95.5,51.5 L78.5,51.5 Z M17.0841793,19.0191036 L19.2054997,16.8977832 L31.226315,28.9185985 L29.1049946,31.0399189 L17.0841793,19.0191036 Z M69.4100811,71.3450054 L71.5314015,69.223685 L83.5522168,81.2445003 L81.4308964,83.3658207 L69.4100811,71.3450054 Z M18.5691036,83.3658207 L16.4477832,81.2445003 L28.4685985,69.223685 L30.5899189,71.3450054 L18.5691036,83.3658207 Z M70.8950054,31.0399189 L68.773685,28.9185985 L80.7945003,16.8977832 L82.9158207,19.0191036 L70.8950054,31.0399189 Z" id="rays" fill="rgb(var(--fg))"></path>
       </svg> 
     </div>
   </header>
@@ -95,6 +93,10 @@ svg path {
   transition: transform 150ms ease-out, opacity 150ms ease-out;
 }
 
+.toggle-light:not(.night) svg path {
+  transition-delay: 125ms;
+}
+
 .toggle-light.night svg path {
   opacity: 0;
   transform: scale(0.8);
@@ -114,10 +116,12 @@ svg path {
 
 .toggle-light.night #cut {
   transform: translate(10px, -10px);
+  transition-delay: 125ms;
 }
 
 .toggle-light.night #orb {
   transform: scale(1.3) translate(30px, 30px);
+  transition-delay: 125ms;
 }
 
 </style>
